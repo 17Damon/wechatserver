a68a806798f9d5406ea7dc210429cb04887ff33a
miss61008596 <61008596@qq.com>
2016-06-17 04:04:36 +0800

    20160617
    start

---
+++ app.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var nodeWeixinAuth = require('node-weixin-auth');
+var settings = require('node-weixin-settings');
+
+var app = {
+    id: 'wxd73dc548a07ac76c',
+    secret: '11507723ee56e2b3f706abc505c62b2b',
+    token: 'zhubg'
+};
+
+//手动得到accessToken
+nodeWeixinAuth.tokenize(settings, app, function (error, json) {
+    var accessToken = json.access_token;
+});
+
+//自动获得accessToken，并发送需要accessToken的请求
+nodeWeixinAuth.determine(settings, app, function () {
+    //这里添加发送请求的代码
+});
+
+//获取服务器IP
+nodeWeixinAuth.ips(settings, app, function (error, data) {
+    //error == false
+    //data.ip_list获取IP列表
+});
+
+
+//与微信对接服务器的验证
+var errors = require('web-errors').errors;
+// var request = require('supertest');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var server = express();
+
+server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.json());
+
+// 微信服务器返回的ack信息是HTTP的GET方法实现的
+server.get('/weixin/ack', function (req, res) {
+    var data = nodeWeixinAuth.extract(req.query);
+    nodeWeixinAuth.ack(app.token, data, function (error, data) {
+        if (!error) {
+            res.send(data);
+            console.log('connetct done!');
+            return;
+        }
+        switch (error) {
+            case 1:
+                res.send(errors.INPUT_INVALID);
+                break;
+            case 2:
+                res.send(errors.SIGNATURE_NOT_MATCH);
+                break;
+            default:
+                res.send(errors.UNKNOWN_ERROR);
+                break;
+        }
+    });
+});
+
+var  listener = server.listen(80, function () {
+    let host = listener.address().address;
+    let port = listener.address().port;
+    console.log('hello,http://' + host + ':' + port);
+});
