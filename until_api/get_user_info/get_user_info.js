ab749a6cd411c077d336b8321c1f2fa9ff959c7d
miss61008596 <61008596@qq.com>
2016-06-18 23:46:29 +0800

    edit

--- until_api/get_user_info/get_user_info.js
+++ until_api/get_user_info/get_user_info.js
@@ -16,27 +16,26 @@ function getAccessToken(appid, secret, code,res) {
     var resCustom = function(obj){
         res.send(obj);
     };
-    let url = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=` + id + `&secret=` + secret + `&code=` + code + `&grant_type=authorization_code`;
-    nodegrass.get(url, function (data, status, headers) {
-        console.log('1.通过code换取网页授权access_token');
-        console.log(status);
-        console.log(headers);
-        console.log(data);
-        if (data.access_token) {
-            access_token = data.access_token;
-            refresh_token = data.refresh_token;
-            openid = data.openid;
+    // let url = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=` + id + `&secret=` + secret + `&code=` + code + `&grant_type=authorization_code`;
+    // nodegrass.get(url, function (data, status, headers) {
+    //     console.log('1.通过code换取网页授权access_token');
+    //     console.log(status);
+    //     console.log(headers);
+    //     console.log(data);
+    //     if (data.access_token) {
+    //         access_token = data.access_token;
+    //         refresh_token = data.refresh_token;
+    //         openid = data.openid;
             id = appid;
-            return checkAccessToken(access_token, openid);
-        } else {
-            console.log('获取access_token失败');
-            return resCustom('获取access_token失败');
-            // return getAccessToken(appid,secret,code);
-        }
-    }, null, 'utf8').on('error', function (e) {
-        console.log("Got error: " + e.message);
-        throw e;
-    });
+            return checkAccessToken('vwRBLoAqhxjwZCsqYlwab4Yfgx1xdpIlmF9LIOdmR-cAV4GVf22L-pChElx4QAuXRDkfe1Kyb2aF0FevYQ40QuOJE3Hnfj2RSotx7s7C8oY', "o7CarwEfoKMx_tWSo54kKFPtkgYA");
+    //     } else {
+    //         console.log('获取access_token失败');
+    //         return resCustom('获取access_token失败');
+    //         // return getAccessToken(appid,secret,code);
+    //     }
+    // }, null, 'utf8').on('error', function (e) {
+    //     console.log("Got error: " + e.message);
+    // });
 }
 
 //检验授权凭证（access_token）是否有效
@@ -47,15 +46,19 @@ function checkAccessToken(access_token, openid) {
         console.log(status);
         console.log(headers);
         console.log(data);
-        if (data.errcode === 0) {
+        let data_json = JSON.parse(data);
+        if (data_json.errcode === 0) {
             effect_flag = true;
+            console.log('access_token有效');
             return getUserinfo(access_token, openid);
-        } else {
-            return refreshAccessToken(refresh_token, id);
+        } else if(data_json.errcode === 42001) {
+            console.log('access_token超时');
+            return refreshAccessToken("vwRBLoAqhxjwZCsqYlwab1VMFcj9SKO-w7DGsFI5xaNgTvbWdxkr_7kcvOipgat6m8iojTYli9NolR4eFmi9TJBW6VksaE1Jys4Rmps5UQ8", id);
+        }else {
+            console.log('access_token和openid不匹配');
         }
     }, null, 'utf8').on('error', function (e) {
         console.log("Got error: " + e.message);
-        throw e;
     });
 
 }
@@ -68,18 +71,20 @@ function refreshAccessToken(refresh_token, id) {
         console.log(status);
         console.log(headers);
         console.log(data);
-        access_token = data.access_token;
-        refresh_token = data.refresh_token;
-        if(errcode){
+        let data_json = JSON.parse(data);
+        access_token = data_json.access_token;
+        refresh_token = data_json.refresh_token;
+        if(data_json.errcode){
             console.log('刷新失败，再次刷新！');
-            return resCustom('刷新access_token失败');
+            // return resCustom('刷新access_token失败');
             // return refreshAccessToken(refresh_token, id);
         }else {
-            return getUserinfo(access_token, openid);
+            console.log('刷新access_token成功！');
+            console.log(data_json);
+            // return getUserinfo(access_token, openid);
         }
     }, null, 'utf8').on('error', function (e) {
         console.log("Got error: " + e.message);
-        throw e;
     });
 }
 
@@ -91,16 +96,18 @@ function getUserinfo(access_token, openid) {
         console.log(status);
         console.log(headers);
         console.log(data);
-        if (errcode){
+        let data_json = JSON.parse(data);
+        if (data_json.errcode){
             console.log('拉取用户信息，再次拉取用户信息！');
-            return resCustom('拉取用户信息失败');
+            // return resCustom('拉取用户信息失败');
             // return getUserinfo(access_token, openid);
         }else {
-            return resCustom(data);
+            // return resCustom(data_json);
+            console.log('拉取用户信息成功！');
+            console.log(data_json);
         }
     }, null, 'utf8').on('error', function (e) {
         console.log("Got error: " + e.message);
-        throw e;
     });
 }
 
