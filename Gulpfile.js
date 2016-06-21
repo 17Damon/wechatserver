ba3cfad46d1959718bcf339b05e35c3aa2592896
miss61008596 <61008596@qq.com>
2016-06-21 19:50:38 +0800

    edit

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,16 @@
-var gulp       = require('gulp'),
-    browserify = require('gulp-browserify');
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify'),
+    uglify = require('gulp-uglify');
 
 gulp.task('scripts', function () {
 
     gulp.src(['app/main.js'])
         .pipe(browserify({
             debug: true,
-            transform: [ 'reactify' ]
+            transform: ['reactify']
         }))
+        //  压缩文件
+        .pipe(uglify())
         .pipe(gulp.dest('./public/'));
 
 });
